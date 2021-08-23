f1e1669f1b1aab6f6e9d6c02772d0dbace42e9fe
prashan133 <dsrprasant@gmail.com>
2021-08-23 10:38:43 +0545

    initial

--- pygame.py
+++ pygame.py
@@ -1,6 +1,8 @@
+
 from tkinter import *
 from PIL import ImageTk ,Image
 import sqlite3
+from create_an_account import reg
 #creating the window
 root = Tk()
 
@@ -8,82 +10,44 @@ root = Tk()
 root.title("Loginpage")
 
 #set the geometry for the root window
-root.geometry('279x180')
+root.geometry('350x320')
 
 #adding the background
-bg = ImageTk.PhotoImage(Image.open("background.jpg"))
+bg = ImageTk.PhotoImage(Image.open("bgg.jpg"))
 
-
-#DATABASES
-#create a databases or connect to one
-conn = sqlite3.connect('Login_book.db')
-
-#create cursor
-c = conn.cursor()
-'''
-#create Table
-c.execute("""CREATE TABLE Login(
-    user text,
-    password text
-)""")
-'''
-#create submit button for databases
-def submit():
-    #create a databases or connect to one
-    conn = sqlite3.connect('Login_book.db')
-
-    #create cursor
-    c = conn.cursor()
-
-    #insert into table
-    c.execute("INSERT INTO Login VALUES (:user,:password)",{
-        'user':user.get(),
-        'password':password.get()
-    })
-
-    #commit change
-    conn.commit()
-
-    #close connection
-    conn.close()
-
-    #clear the text boxes
-    user.delete(0,END)
-    password.delete(0,END)
 
 #create a label
 my_label = Label(root,image=bg)
 my_label.place(x=0,y=0,relwidth=1,relheight=1)
 
 #add something to the top of our image
-my_text = Label(root,text="welcome",font=("helvetica",15) ,fg ="#235f73",bg='#58d3ff')
+my_text = Label(root,text="Login",font=("helvetica",15),bg='#b9f1f0')
 my_text.pack(pady=20)
 
 # create a frame
-my_frame = Frame(root)
+my_frame = Frame(root,bg='#b9f1f0')
 my_frame.pack()
 
 #creating label
-user_name = Label(my_frame,text='UserName',bg="#9ce0f6")
+user_name = Label(my_frame,text='UserName',bg="#ffb347",bd=4)
 user_name.grid(row=0,column=0)
-password = Label(my_frame,text='Password',bg='#368872')
-password.grid(row =1 , column =0)
+password = Label(my_frame,text='Password',bg='#ffb347',bd=4)
+password.grid(row =2 , column =0)
 
 #creating textbox
-user = Entry(my_frame,width=30,bg='#9ce0f6')
-user.grid(row=0,column = 2 )
-password = Entry(my_frame,width=30,bg="#368872")
-password.grid(row=1 ,column=2)
+user = Entry(my_frame,width=30,bg='#b9f1f0',bd=4)
+user.grid(row=0,column = 2 ,padx=10,pady=10)
+password = Entry(my_frame,width=30,bg="#b9f1f0",bd=4)
+password.grid(row=2 ,column=2,padx=10,pady=10)
 
-#creating the button for submit
-submit_btn = Button(my_frame,text='Submit',bg='#486577',command=submit)
-submit_btn.grid(row=2,column = 2, columnspan=2)
 
-#commit change
-conn.commit()
+#creting button to open an account
+create_button = Button(my_frame,text="create an account",bg='#ffb347',command=reg)
+create_button.grid(row=3,column=2)
+#creating sumbit button
+submit_button = Button(my_frame,text="login",bd=6,bg="#ffb347")
+submit_button.grid(row=5 ,column = 2,pady=10 )
 
-#close connection
-conn.close()
 
 
 
